2819bd83ce6a2d52540e1abc53f70ffcb1cffcbc
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2021-06-17 12:09:55 +0530

    refactor: :art: refactor code design and merged #8
    added @types/jest to tsconfig types and removed unwanted code from PR #8

--- src/__tests__/Genesis.test.ts
+++ src/__tests__/Genesis.test.ts
@@ -1,11 +1,6 @@
 import Request from 'supertest';
 import { app } from '../app';
 
-// Removing these will cause type error for tests
-// Alternatively, commenting typeRoots in tsconfig also fix this problem
-import 'mocha';
-import '@types/jest';
-
 describe('Testing http:api calls', () => {
   it('should return 200 with valid response', async () => {
     const data = await Request(app).get('/').expect('Content-Type', /json/).expect(200);
